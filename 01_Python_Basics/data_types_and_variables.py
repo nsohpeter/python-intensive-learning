ee70d0275b1723e0ad0d2148cb3ec0e53ecbbcd9
nsohpeter <apiahpetertoch@gmail.com>
2026-02-16 20:41:58 +0800

    commiting changes

---
+++ 01_Python_Basics/data_types_and_variables.py
@@ -0,0 +1,9 @@
+print("Variables in python")
+
+print("*****************************")
+# VARIABLES ION PYTHON
+name = "peter"
+# print(name)
+
+greeting = "good morning"
+print("hello," + greeting + " " + name.title())
---
+++ 01_Python_Basics/temporal.py
@@ -0,0 +1,14 @@
+# checking the type of of a variable
+
+name = "peter"
+index_num = 202524090121
+checked = True
+decimal = 1.24568
+another_number = 1.5
+print("\n")
+print(type(name))
+print(type(index_num))
+print(type(checked))
+print(type(decimal))
+print(type(another_number))
+print("\n")
